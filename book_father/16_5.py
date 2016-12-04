64194cf396d65d9f983fc0b64a5bfb49887fd6df
qinfeng233 <24525547@qq.com>
2016-12-04 21:34:24 +0800

    图形

--- book_father/16_5.py
+++ book_father/16_5.py
@@ -8,10 +8,12 @@ for i in range(100):
     top = random.randint(0,400)
     left = random.randint(0,500)
     pygame.draw.rect(screen,[0,255,0],[top,left,height,width],1)
-pygame.display.flip()
+#pygame.display.flip()
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    pygame.display.flip()
+    pygame.time.delay(30)
 pygame.quit()
